8d52d20577f98350b49cb73455a9d2e00e4c3408
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-04-08 14:14:00 +0530

    Musicplay.py

---
+++ Lyci/modules/musicplay.py
@@ -0,0 +1,13 @@
+__help__ = """
+*Lyci - A Powerful Telegram Group Manager 🎶
+Powerful Abilities*
+• Group Voice Chat Music Play ❤
+/play - 🎶 Play the replied audio file or YouTube video \n
+/pause - ▶️ Pause the audio stream \n
+/resume - ⏸ Resume the audio stream \n
+/skip - ↪️ Skip the current audio stream \n
+/mute - 🔇 Mute the userbot \n
+/unmute - 🔊 Unmute the userbot \n
+/stop - 🗑🛑 Clear the queue and remove the userbot from the call"""
+
+__mod_name__ = "Music Play 🎶"
