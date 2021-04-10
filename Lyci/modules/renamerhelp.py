c6532aa40ee35666782bddf76f5e308d66b2d67f
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-04-11 02:36:06 +0530

    Update renamerhelp.py

--- Lyci/modules/renamerhelp.py
+++ Lyci/modules/renamerhelp.py
@@ -3,9 +3,9 @@ __help__ = """
 
 Just reply to file with below commands*
 
-•  /rename `newfilename.extension` - rename media in as file
-•  /rename_video `newfilename.extension` - rename media as streamable video
+•  /rename newfilename.extension  - rename media in as file
+•  /rename_video newfilename.extension  - rename media as streamable video
 
-© @LyciBot 👸"""
+© @LyciBot 🤖"""
 
 __mod_name__ = "File Renamer ✏"
